68357092d2a615338f175859132ebc65e59f3566
Richard Spiers <richard@thoughtexpress.com>
2012-10-05 17:52:59 +0200

    Beginning of SRS
    Empty framework for SRS

---
+++ SRS/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using SIPLib.SIP;
+using SIPLib.Utils;
+using log4net;
+
+namespace SRS
+{
+    class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(SIPApp));
+        private static readonly ILog SessionLog = LogManager.GetLogger("SessionLogger");
+        private static SIPApp _app;
+        public static SIPStack CreateStack(SIPApp app, string proxyIp = null, int proxyPort = -1)
+        {
+            SIPStack myStack = new SIPStack(app);
+            if (proxyIp != null)
+            {
+                myStack.ProxyHost = proxyIp;
+                myStack.ProxyPort = (proxyPort == -1) ? 5060 : proxyPort;
+            }
+            return myStack;
+        }
+
+        public static TransportInfo CreateTransport(string listenIp, int listenPort)
+        {
+            return new TransportInfo(IPAddress.Parse(listenIp), listenPort, System.Net.Sockets.ProtocolType.Udp);
+        }
+
+        static void AppResponseRecvEvent(object sender, SipMessageEventArgs e)
+        {
+            Log.Info("Response Received:" + e.Message);
+            Message response = e.Message;
+            string requestType = response.First("CSeq").ToString().Trim().Split()[1].ToUpper();
+            switch (requestType)
+            {
+                case "INVITE":
+                case "REGISTER":
+                case "BYE":
+                default:
+                    Log.Info("Response for Request Type " + requestType + " is unhandled ");
+                    break;
+            }
+        }
+
+        static void AppRequestRecvEvent(object sender, SipMessageEventArgs e)
+        {
+            Log.Info("Request Received:" + e.Message);
+            Message request = e.Message;
+            switch (request.Method.ToUpper())
+            {
+                case "MESSAGE":
+                    {
+                        _app.Useragents.Add(e.UA);
+                        Log.Info("Received Message with body:" + e.Message.Body);
+                        Message m = e.UA.CreateResponse(200, "OK");
+                        e.UA.SendResponse(m);
+                        break;
+                    }
+                case "INVITE":
+                case "BYE":
+                case "CANCEL":
+                case "ACK":
+                case "OPTIONS":
+                case "REFER":
+                case "SUBSCRIBE":
+                case "NOTIFY":
+                case "PUBLISH":
+                case "INFO":
+                default:
+                    {
+                        Log.Info("Request with method " + request.Method.ToUpper() + " is unhandled");
+                        break;
+                    }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TransportInfo localTransport = CreateTransport(Helpers.GetLocalIP(), 7000);
+            _app = new SIPApp(localTransport);
+            _app.RequestRecvEvent += new EventHandler<SipMessageEventArgs>(AppRequestRecvEvent);
+            _app.ResponseRecvEvent += new EventHandler<SipMessageEventArgs>(AppResponseRecvEvent);
+            const string scscfIP = "scscf.open-ims.test";
+            const int scscfPort = 6060;
+            SIPStack stack = CreateStack(_app, scscfIP, scscfPort);
+            stack.Uri = new SIPURI("service_repository@open-ims.test");
+            Console.ReadKey();
+        }
+    }
+}
